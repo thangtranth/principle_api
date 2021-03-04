733dae71dbe4fcd5bfde41125acb8ca281a38bf1
Thang Tran <thangtran@brodev.com>
2021-03-04 16:53:08 +0700

    change port

--- main.py
+++ main.py
@@ -54,4 +54,7 @@ def get_principle():
 #     return jsonify(search.query(key_word))
 
 
-app.run(port=5000)
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
