cb5942706fb7b62790035f7edf450fef610f71d9
Thang Tran <thangtran@brodev.com>
2021-03-04 17:17:07 +0700

    change port

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ from mysql.connector import Error
 # import key_word_search
 
 app = flask.Flask(__name__)
-app.config['Debug'] = True
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='principles',
-                                         user='root')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-
+print("here")
+# app.config['Debug'] = True
+# try:
+#     connection = mysql.connector.connect(host='localhost',
+#                                          database='principles',
+#                                          user='root')
+#     if connection.is_connected():
+#         db_Info = connection.get_server_info()
+#         print("Connected to MySQL Server version ", db_Info)
+#         cursor = connection.cursor()
+#         cursor.execute("select database();")
+#         record = cursor.fetchone()
+#         print("You're connected to database: ", record)
+#
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
+#
 
 @app.route('/')
 def hello():
