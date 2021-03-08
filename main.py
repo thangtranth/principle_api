8d62936eaaa986e4c77fe8ba3331e0cbc3e63c09
Thang Tran <thangtran@brodev.com>
2021-03-08 11:24:05 +0700

    change port

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def get_para():
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
